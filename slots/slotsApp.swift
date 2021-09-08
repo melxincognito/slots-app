ddeee4cbe0d05ea462aa7de027963827386802b3
Mel Incognito <melxincognito@gmail.com>
2021-09-08 11:55:31 -0700

    Initial Commit

---
+++ slots/slotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotsApp.swift
+//  slots
+//
+//  Created by Mel on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct slotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
