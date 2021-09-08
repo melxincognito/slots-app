cae39afc96b221ff8c53da27f60cc43bfc3f763a
Mel Incognito <melxincognito@gmail.com>
2021-09-08 13:10:02 -0700

    'slots app'

--- slots/ContentView.swift
+++ slots/ContentView.swift
@@ -8,9 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var cherryCard = "cherry"
+    @State private var appleCard = "apple"
+    @State private var starCard = "Star"
+    @State private var points = 0
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            
+            Image("gold")
+            
+            
+            VStack {
+                Spacer()
+        
+                Text("S l o t s")
+                Text("Push the button and try to get three of a kind!")
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("I'm feeling lucky")
+                })
+                
+                
+                
+                    HStack {
+                    
+                        Image("cherry").resizable()
+                        
+                            
+                        Image("apple").resizable()
+                        
+                        
+                        Image("star").resizable()
+                        
+                        
+                 
+                    }
+                    .frame(width: 320, height: 120).padding(.vertical, 100.0)
+                    
+                
+                
+                
+                Text("Your points:")
+                
+                Spacer()
+                
+                    
+                
+            }
+            
+        }
+        
     }
 }
 
